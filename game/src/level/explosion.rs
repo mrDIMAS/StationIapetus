b99c1d8d41b21514709d06d4236bce7078200e88
Dmitry <d1maxa@yandex.ru>
2023-07-07 19:56:48 +0300

    fixed explosion

--- game/src/level/explosion.rs
+++ game/src/level/explosion.rs
@@ -1,6 +1,7 @@
 use fyrox::{
     core::{
-        algebra::Vector3,
+        algebra::{Matrix4, Vector3},
+        math::aabb::AxisAlignedBoundingBox,
         reflect::prelude::*,
         uuid::{uuid, Uuid},
         variable::InheritableVariable,
@@ -12,9 +13,21 @@ use fyrox::{
     script::{ScriptContext, ScriptTrait},
 };
 
-#[derive(Visit, Reflect, Default, Debug, Clone)]
+#[derive(Visit, Reflect, Debug, Clone)]
 pub struct Explosion {
+    #[visit(optional)]
     strength: InheritableVariable<f32>,
+    #[visit(optional)]
+    scale: InheritableVariable<Vector3<f32>>,
+}
+
+impl Default for Explosion {
+    fn default() -> Self {
+        Self {
+            strength: 100.0f32.into(),
+            scale: Vector3::new(2.0, 2.0, 2.0).into(),
+        }
+    }
 }
 
 impl_component_provider!(Explosion);
@@ -28,7 +41,8 @@ impl TypeUuidProvider for Explosion {
 impl ScriptTrait for Explosion {
     fn on_start(&mut self, context: &mut ScriptContext) {
         let node = &context.scene.graph[context.handle];
-        let aabb = node.world_bounding_box();
+        let aabb = AxisAlignedBoundingBox::unit()
+            .transform(&(node.global_transform() * Matrix4::new_nonuniform_scaling(&*self.scale)));
         let center = aabb.center();
         for rigid_body in context
             .scene
@@ -36,10 +50,14 @@ impl ScriptTrait for Explosion {
             .linear_iter_mut()
             .filter_map(|n| n.query_component_mut::<RigidBody>())
         {
-            let d = rigid_body.global_position() - center;
-            let k = d.component_div(&aabb.half_extents());
-            let force = (Vector3::repeat(1.0) - k.inf(&Vector3::repeat(1.0))).scale(*self.strength);
-            rigid_body.apply_force(force);
+            if aabb.is_contains_point(rigid_body.global_position()) {
+                let d = rigid_body.global_position() - center;
+                let k = (Vector3::repeat(1.0) - d.component_div(&aabb.half_extents()))
+                    .scale(*self.strength);
+                let force = d.normalize().component_mul(&k);
+                rigid_body.apply_force(force);
+                rigid_body.wake_up();
+            }
         }
     }
 
